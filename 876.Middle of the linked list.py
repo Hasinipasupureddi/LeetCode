7a80ea3ee0cd01d9b2e4ca6fdcf88d58b8920fa7
Hasinipasupureddi <hasini.dsl.pasupureddi@gmail.com>
2025-06-11 18:59:59 +0530

    876.Middle of the linked list.py
    Solution of LeetCode problem 876

---
+++ 876.Middle of the linked list.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def middleNode(self, head):
+       slow=fast=head
+       while fast and fast.next:
+        fast=fast.next.next
+        slow=slow.next
+       return slow
