5252cabac7413ddd0fb4008c08b8244131ba715f
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-10-30 21:58:05 +0530

    Implement 2413.smallestEvenMultiple  function
    2413.Smallest even multiple-leetcode

---
+++ 2413.Smallest even multiple.py
@@ -0,0 +1,8 @@
+#2413.Smallest even multiple- leetcode
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n%2==0:
+            return n
+        else :
+            return n*2
+        
