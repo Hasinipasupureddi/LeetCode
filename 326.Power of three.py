6011c1fb1a616de8bedf9b873cddfdae42aaa99d
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-11-01 00:20:14 +0530

    Add solution for checking power of three-326-leetcode

---
+++ 326.Power of three.py
@@ -0,0 +1,8 @@
+#326.Power of three-leetcode
+class Solution:
+    def isPowerOfThree(self, n: int) -> bool:
+        if n<=0:
+            return False
+        while n%3==0:
+            n=n//3
+        return n==1
