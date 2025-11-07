363c64a091f753b0d6617e813b63e929b5921734
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-11-07 22:46:35 +0530

    Implement missing number solution in Python

---
+++ 368.Missing Number.py
@@ -0,0 +1,6 @@
+#268.Missing number- leetcode
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        total = n * (n + 1) // 2
+        return total - sum(nums)
