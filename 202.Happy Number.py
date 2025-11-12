cc587050eb5b4e71bb27ad5b143ad11fec2a77a8
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-11-12 17:58:22 +0530

    Add implementation for Happy Number problem-202-leetcode

---
+++ 202.Happy Number.py
@@ -0,0 +1,12 @@
+#202.Happy Number-leetcode
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        seen = set()
+        while n != 1 and n not in seen:
+            seen.add(n)
+            temp = 0
+            while n:
+                temp += (n % 10) ** 2
+                n //= 10
+            n = temp
+        return n == 1
