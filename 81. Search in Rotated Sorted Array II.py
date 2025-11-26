0bb14b36e3ec4907f2b3ae60296cdfb1b967059d
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-11-26 13:27:02 +0530

    81. Search in Rotated Sorted Array II-leetcode

---
+++ 81. Search in Rotated Sorted Array II.py
@@ -0,0 +1,27 @@
+#81. Search in Rotated Sorted Array II-leetcode
+class Solution:
+    def search(self, nums: List[int], target: int) -> bool:
+        n = len(nums)
+        low = 0
+        high = n - 1
+        while low <= high:
+            mid = (low + high) // 2
+            if nums[mid] == target:
+                return True
+            # Handle duplicates
+            if nums[low] == nums[mid] == nums[high]:
+                low += 1
+                high -= 1
+            # Left half sorted
+            elif nums[low] <= nums[mid]:
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            # Right half sorted
+            else:  # nums[mid] <= nums[high]
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+        return False
