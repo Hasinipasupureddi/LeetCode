d5b32cfcad339309ca34335e8f4b19a893a07729
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2025-11-30 23:24:56 +0530

    541. Reverse String II-leetcode

---
+++ 541. Reverse String II.py
@@ -0,0 +1,7 @@
+#541. Reverse String II-leetcode
+class Solution:
+    def reverseStr(self, s: str, k: int) -> str:
+        s = list(s)
+        for i in range(0, len(s), 2 * k):
+            s[i : i + k] = reversed(s[i : i + k])
+        return "".join(s)
