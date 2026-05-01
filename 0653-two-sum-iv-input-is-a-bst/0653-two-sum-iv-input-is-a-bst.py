4bd105b891d1924445741d206187e4a4d37d9e50
PASUPUREDDI HASINI <206112299+Hasinipasupureddi@users.noreply.github.com>
2026-04-30 23:27:47 -0700

    Time: 7 ms (43.2%), Space: 21.9 MB (79.74%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.py
@@ -6,7 +6,7 @@
 #         self.right = right
 class Solution:
     def findTarget(self, root: Optional[TreeNode], k: int) -> bool:
-        seen=set()
+        '''seen=set()
         def dfs(node):
             if not node:
                 return False
@@ -14,4 +14,24 @@ class Solution:
                 return True
             seen.add(node.val)
             return dfs(node.left) or dfs(node.right)
-        return dfs(root)
+        return dfs(root)'''
+        
+        def inorder(node,arr):
+            if not node:
+                return None
+            inorder(node.left,arr)
+            arr.append(node.val)
+            inorder(node.right,arr)
+        arr=[]
+        inorder(root,arr)
+        l=0
+        r=len(arr)-1
+        while l<r:
+            s=arr[l]+arr[r]
+            if s==k:
+                return True
+            elif s<k:
+                l+=1
+            else:
+                r-=1
+        return False
